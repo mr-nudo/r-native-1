eb04bdf92e7362125d0d493279773ab415656554
mr-nudo <odunayo.adekoya@yahoo.com>
2021-11-26 09:40:22 +0100

    Added Head, Tail & Food classes

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from "./Constants";
+import Head from "./components/Head";
 
 export default function App() {
   const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
@@ -16,6 +17,17 @@ export default function App() {
           height: BoardSize,
           flex: null,
           backgroundColor: "white",
+        }}
+        entities={{
+          head: {
+            position: [0,0],
+            size: Constants.CELL_SIZE,
+            updateFrequency: 10,
+            nextMove: 10,
+            xspeed: 0,
+            yspeed: 0,
+            renderer: <Head />,
+          }
         }}
       />
     </View>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # r-native-1
+
+Awesome tutorial here : https://blog.logrocket.com/react-native-game-development-tutorial/
