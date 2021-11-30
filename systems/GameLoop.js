feab252954ed54d9135a1ce4dfe8263ee09dbc8b
mr-nudo <odunayo.adekoya@yahoo.com>
2021-11-30 07:48:33 +0100

    Added Game Over Logic

--- systems/GameLoop.js
+++ systems/GameLoop.js
@@ -4,8 +4,16 @@ export default function (entities, { events, dispatch }) {
     head.nextMove -= 1;
     if (head.nextMove === 0) {
         head.nextMove = head.updateFrequency;
-
-        head.position[0] += head.xspeed;
-        head.position[1] += head.yspeed;
+        if (
+              head.position[0] + head.xspeed < 0 ||
+              head.position[0] + head.xspeed >= Constants.GRID_SIZE ||
+              head.position[1] + head.yspeed < 0 ||
+              head.position[1] + head.yspeed >= Constants.GRID_SIZE
+            ) {
+              dispatch("game-over");
+            } else {
+              head.position[0] += head.xspeed;
+              head.position[1] += head.yspeed;
+          }
     }
 }
